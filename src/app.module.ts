57ad974b851e478ce293606915150bb0a2ac88a5
HugoMzFernandes <hugomzfernandes@gmail.com>
2024-05-20 20:50:28 -0300

    feat(cache): adding redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PokedexModule } from './modules/pokedex/pokedex.module';
 import { ConfigModule } from '@nestjs/config'
+import { CacheModule } from '@nestjs/cache-manager'
+import * as redisStore from 'cache-manager-redis-store'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      no_ready_check: true,
+    }),
     PokedexModule,
   ],
 })
--- src/modules/pokedex/pokedex.service.ts
+++ src/modules/pokedex/pokedex.service.ts
@@ -1,20 +1,32 @@
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable} from "@nestjs/common";
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from "cache-manager"
 import { BaseService } from "src/shared/services/base.service";
-import { Observable, map } from "rxjs";
+import { Observable, map, tap, of } from "rxjs";
+
 import { ResultViewModel } from "src/shared";
 import { PokemonAdaptor } from "./adaptors/pokemon-data.adaptor";
-import { PokemonDataDto } from "./models";
+import { PokemonDataDto, PokemonDataViewModel } from "./models";
 
 @Injectable()
 export class PokedexService extends BaseService {
-  constructor(private readonly httpService: HttpService) {
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+  ) {
     super('https://pokeapi.co/api/v2/pokemon')
   }
 
-  getPokemonByNameData(name: string): Observable<ResultViewModel<PokemonDataDto>> {
+  async getPokemonByNameData(name: string): Promise<Observable<ResultViewModel<PokemonDataDto>>> {
+    const cachedPokemonData: PokemonDataViewModel = await this.cacheService.get(name)
+    if (cachedPokemonData) {
+      return of(PokemonAdaptor.pokemonData(cachedPokemonData) as unknown as ResultViewModel<PokemonDataDto>);
+    }
     return this.httpService
-      .get(this.getApiUrl(`${name}`))
-      .pipe(map((data) => PokemonAdaptor.pokemonData(data?.data)))
+      .get(this.getApiUrl(`${name}`)).pipe(
+        tap((data) => this.cacheService.set(name, data?.data)),
+        map((data) => PokemonAdaptor.pokemonData(data?.data))
+      );
   }
 }
